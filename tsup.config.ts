9c80f9814a4f481ba575da51774a487bcb97fcca
Julian Wowra <development@julianwowra.de>
2025-05-04 15:08:53 +0200

    chore: use `tsup` to build `esm` and `cjs`

---
+++ tsup.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig, type Options } from 'tsup';
+import pkg from './package.json';
+
+const bannerComment = `/*
+ * 📦 ${pkg.name}
+ *
+ * 🏷️ Version:    ${pkg.version}
+ * 📄 License:    ${pkg.license}
+ * 🕒 Build:      ${new Date().toISOString()}
+ * 🔗 Repository: ${pkg.repository}
+ * 👤 Author:     ${pkg.author}
+ */\n`;
+
+const sharedConfig: Options = {
+	tsconfig: './tsconfig.json',
+	entry: ['src/index.ts'],
+	outDir: 'dist',
+	target: 'es2024',
+
+	clean: true,
+	minify: true,
+	treeshake: true,
+	skipNodeModulesBundle: true,
+	banner: {
+		js: bannerComment
+	}
+};
+
+export default defineConfig([
+	// ESM
+	{
+		...sharedConfig,
+		format: 'esm',
+		platform: 'neutral',
+		outDir: sharedConfig.outDir + '/esm',
+		splitting: true,
+		dts: false,
+		outExtension: () => ({ js: '.mjs' })
+	},
+
+	// CommonJS
+	{
+		...sharedConfig,
+		format: 'cjs',
+		platform: 'node',
+		outDir: sharedConfig.outDir + '/cjs',
+		splitting: false,
+		dts: false,
+		outExtension: () => ({ js: '.cjs' })
+	},
+
+	// Types
+	{
+		...sharedConfig,
+		outDir: sharedConfig.outDir + '/types',
+		splitting: false,
+		dts: {
+			banner: bannerComment,
+			only: true
+		},
+		outExtension: () => ({ dts: '.d.ts' })
+	}
+]);
